1d0ca86edc2c4d5d0df05f23318fcf77534428c5
cn.yimian.xyz <i@iotcat.me>
2020-06-19 17:31:55 +0800

    ini

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+include './functions.php';
+
+
+Header("HTTP/1.1 301 Moved Permanently"); 
+echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+
+if(!isset($_REQUEST['s'])) {
+    Header("Location: https://www.eee.dog/");
+    die();
+}
+
+$keyword = substr($_REQUEST['s'], 1);
+
+$conn = db__connect();
+
+$res = db__getData($conn, "reflect", "keyword", $keyword);
+
+
+if($res === 404){
+    Header("Location: https://www.eee.dog/");
+    die();
+}
+
+$cmd = "Location: ".$res[0]['url'];
+Header($cmd);
+die();

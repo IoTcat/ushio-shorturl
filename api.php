c055c499958b254e8119fa9b71a74d9831f8c0fa
cn.yimian.xyz <i@iotcat.me>
2020-07-10 10:55:00 +0800

    works

---
+++ api.php
@@ -0,0 +1,15 @@
+<?php
+include './functions.php';
+
+header("Access-Control-Allow-Origin: *");
+$url = $_REQUEST['url'];
+
+$url = base64_decode($url);
+
+if(!isset($url)) die();
+
+$key = substr(md5($url+time()), 0, 6);
+db__pushData(db__connect(), "reflect", array("keyword"=>$key, "type"=>"auto", "url"=>$url, "insert_at"=>date("Y-m-d H:i:s")));
+
+
+echo json_encode(array("key"=>"http://eee.dog/".$key));
